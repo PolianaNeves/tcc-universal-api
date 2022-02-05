d8d60dc9dd3ff2f26325f6ea7ca820f5c945fd7a
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2022-02-04 21:41:43 -0300

    remove duplicated code

--- controllers/attractions.py
+++ controllers/attractions.py
@@ -6,10 +6,3 @@ def get_reviews_count_by_attraction(branch, label=""):
     if label != "":
         column_filter = f'{branch}_count_{label}'
     return count_reviews_by_attraction(column_filter)
-
-
-def get_reviews_count_by_attraction(branch, label=""):
-    column_filter = f'{branch}_count'
-    if label != "":
-        column_filter = f'{branch}_count_{label}'
-    return count_reviews_by_attraction(column_filter)
