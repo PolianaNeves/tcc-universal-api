d570d6be5f469e6663b8e90af3a79802a3d86ddb
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2022-02-05 19:40:14 -0300

    removing unused datasets and uploading assets folder

--- inits/init_dataset.py
+++ inits/init_dataset.py
@@ -4,5 +4,4 @@ import os
 
 dataset_completed = pd.read_csv(os.path.join("./dataset", "dataset_completed.csv"))
 dataset_with_attractions = pd.read_csv(os.path.join("./dataset", "dataset_completed_attractions.csv"))
-attractions_list = pd.read_csv(os.path.join("./dataset", "attractions_list.csv"))
 dataset_attractions_count = pd.read_csv(os.path.join("./dataset", "dataset_attractions_count.csv"))
